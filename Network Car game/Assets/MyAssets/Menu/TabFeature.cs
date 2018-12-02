64e291bdb2e8ff5228182219ae8224d1719202ab
PaulConstable <PaulConstable@users.noreply.github.com>
2018-12-02 15:27:10 +0000

    Tab

---
+++ Network Car game/Assets/MyAssets/Menu/TabFeature.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class TabFeature : MonoBehaviour {
+
+    public InputField nextFields;
+    InputField myField;
+	// Use this for initialization
+	void Start () {
+        if (nextFields == null)
+        {
+            Destroy(this);
+            return;
+        }
+        myField = GetComponent<InputField>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myField.isFocused && Input.GetKeyDown(KeyCode.Tab))
+        {
+            nextFields.ActivateInputField();
+        }
+	}
+}
